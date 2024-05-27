d52a88cd15c136f34ce8b7eed576253c92ee84c0
Carlo Teran <carloteran@Carlos-MacBook-Air.local>
2024-05-27 15:08:12 -0600

    Add Catalyst Dashboard UI

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,125 +1,221 @@
-import { useState } from 'react';
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import Dropdown from '@/Components/Dropdown';
-import NavLink from '@/Components/NavLink';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
-import { Link } from '@inertiajs/react';
+import { Avatar } from '@/Components/avatar'
+import {
+  Dropdown,
+  DropdownButton,
+  DropdownDivider,
+  DropdownItem,
+  DropdownLabel,
+  DropdownMenu,
+} from '@/Components/dropdown'
+import { SidebarLayout } from '@/Components/sidebar-layout'
+import { Navbar, NavbarItem, NavbarSection, NavbarSpacer } from '@/Components/navbar'
+import {
+  Sidebar,
+  SidebarBody,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarHeading,
+  SidebarItem,
+  SidebarLabel,
+  SidebarSection,
+  SidebarSpacer,
+} from '@/Components/sidebar'
+import * as Headless from '@headlessui/react'
+import {
+  ArrowRightStartOnRectangleIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  Cog8ToothIcon,
+  LightBulbIcon,
+  PlusIcon,
+  ShieldCheckIcon,
+  UserIcon,
+} from '@heroicons/react/16/solid'
+import {
+  Cog6ToothIcon,
+  HomeIcon,
+  InboxIcon,
+  MagnifyingGlassIcon,
+  MegaphoneIcon,
+  QuestionMarkCircleIcon,
+  SparklesIcon,
+  Square2StackIcon,
+  TicketIcon,
+} from '@heroicons/react/20/solid'
+
 
 export default function Authenticated({ user, children }) {
-    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
-
     return (
-        <div className="min-h-screen bg-gray-100">
-            <nav className="bg-white border-b border-gray-100">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="flex justify-between h-16">
-                        <div className="flex">
-                            <div className="shrink-0 flex items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                                </Link>
-                            </div>
-
-                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                                    Dashboard
-                                </NavLink>
-                                <NavLink href={route('bookmarks.index')} active={route().current('bookmarks.index')}>
-                                    Bookmarks
-                                </NavLink>
-                            </div>
-                        </div>
-
-                        <div className="hidden sm:flex sm:items-center sm:ms-6">
-                            <div className="ms-3 relative">
-                                <Dropdown>
-                                    <Dropdown.Trigger>
-                                        <span className="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {user.name}
-
-                                                <svg
-                                                    className="ms-2 -me-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </Dropdown.Trigger>
-
-                                    <Dropdown.Content>
-                                        <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
-                                        <Dropdown.Link href={route('logout')} method="post" as="button">
-                                            Log Out
-                                        </Dropdown.Link>
-                                    </Dropdown.Content>
-                                </Dropdown>
-                            </div>
-                        </div>
-
-                        <div className="-me-2 flex items-center sm:hidden">
-                            <button
-                                onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
-                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                            >
-                                <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path
-                                        className={!showingNavigationDropdown ? 'inline-flex' : 'hidden'}
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        className={showingNavigationDropdown ? 'inline-flex' : 'hidden'}
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
-                    <div className="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
-                            Dashboard
-                        </ResponsiveNavLink>
-                        <ResponsiveNavLink href={route('bookmarks.index')} active={route().current('bookmarks.index')}>
-                            Bookmarks
-                        </ResponsiveNavLink>
-                    </div>
-
-                    <div className="pt-4 pb-1 border-t border-gray-200">
-                        <div className="px-4">
-                            <div className="font-medium text-base text-gray-800">{user.name}</div>
-                            <div className="font-medium text-sm text-gray-500">{user.email}</div>
-                        </div>
-
-                        <div className="mt-3 space-y-1">
-                            <ResponsiveNavLink href={route('profile.edit')}>Profile</ResponsiveNavLink>
-                            <ResponsiveNavLink method="post" href={route('logout')} as="button">
-                                Log Out
-                            </ResponsiveNavLink>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
-            <main>{children}</main>
-        </div>
+      <SidebarLayout
+        navbar={
+          <Navbar>
+            <NavbarSpacer />
+            <NavbarSection>
+              <NavbarItem href="/search" aria-label="Search">
+                <MagnifyingGlassIcon />
+              </NavbarItem>
+              <NavbarItem href="/inbox" aria-label="Inbox">
+                <InboxIcon />
+              </NavbarItem>
+              <Dropdown>
+                <DropdownButton as={NavbarItem}>
+                  <Avatar src="/profile-photo.jpg" square />
+                </DropdownButton>
+                <DropdownMenu className="min-w-64" anchor="bottom end">
+                  <DropdownItem href="/my-profile">
+                    <UserIcon />
+                    <DropdownLabel>My profile</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownItem href="/settings">
+                    <Cog8ToothIcon />
+                    <DropdownLabel>Settings</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownDivider />
+                  <DropdownItem href="/privacy-policy">
+                    <ShieldCheckIcon />
+                    <DropdownLabel>Privacy policy</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownItem href="/share-feedback">
+                    <LightBulbIcon />
+                    <DropdownLabel>Share feedback</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownDivider />
+                  <DropdownItem href="/logout">
+                    <ArrowRightStartOnRectangleIcon />
+                    <DropdownLabel>Sign out</DropdownLabel>
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </NavbarSection>
+          </Navbar>
+        }
+        sidebar={
+          <Sidebar>
+            <SidebarHeader>
+              <Dropdown>
+                <DropdownButton as={SidebarItem} className="lg:mb-2.5">
+                  <Avatar src="/tailwind-logo.svg" />
+                  <SidebarLabel>Tailwind Labs</SidebarLabel>
+                  <ChevronDownIcon />
+                </DropdownButton>
+                <DropdownMenu className="min-w-80 lg:min-w-64" anchor="bottom start">
+                  <DropdownItem href="/teams/1/settings">
+                    <Cog8ToothIcon />
+                    <DropdownLabel>Settings</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownDivider />
+                  <DropdownItem href="/teams/1">
+                    <Avatar slot="icon" src="/tailwind-logo.svg" />
+                    <DropdownLabel>Tailwind Labs</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownItem href="/teams/2">
+                    <Avatar slot="icon" initials="WC" className="bg-purple-500 text-white " />
+                    <DropdownLabel>Workcation</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownDivider />
+                  <DropdownItem href="/teams/create">
+                    <PlusIcon />
+                    <DropdownLabel>New team&hellip;</DropdownLabel>
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+              <SidebarSection className="max-lg:hidden">
+                <SidebarItem href="/search">
+                  <MagnifyingGlassIcon />
+                  <SidebarLabel>Search</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href="/inbox">
+                  <InboxIcon />
+                  <SidebarLabel>Inbox</SidebarLabel>
+                </SidebarItem>
+              </SidebarSection>
+            </SidebarHeader>
+            <SidebarBody>
+              <SidebarSection>
+                <SidebarItem href="/">
+                  <HomeIcon />
+                  <SidebarLabel>Home</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href="/events">
+                  <Square2StackIcon />
+                  <SidebarLabel>Events</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href="/orders">
+                  <TicketIcon />
+                  <SidebarLabel>Orders</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href="/settings">
+                  <Cog6ToothIcon />
+                  <SidebarLabel>Settings</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href="/broadcasts">
+                  <MegaphoneIcon />
+                  <SidebarLabel>Broadcasts</SidebarLabel>
+                </SidebarItem>
+              </SidebarSection>
+              <SidebarSection className="max-lg:hidden">
+                <SidebarHeading>Upcoming Events</SidebarHeading>
+                <SidebarItem href="/events/1">Bear Hug: Live in Concert</SidebarItem>
+                <SidebarItem href="/events/2">Viking People</SidebarItem>
+                <SidebarItem href="/events/3">Six Fingers — DJ Set</SidebarItem>
+                <SidebarItem href="/events/4">We All Look The Same</SidebarItem>
+              </SidebarSection>
+              <SidebarSpacer />
+              <SidebarSection>
+                <SidebarItem href="/support">
+                  <QuestionMarkCircleIcon />
+                  <SidebarLabel>Support</SidebarLabel>
+                </SidebarItem>
+                <SidebarItem href="/changelog">
+                  <SparklesIcon />
+                  <SidebarLabel>Changelog</SidebarLabel>
+                </SidebarItem>
+              </SidebarSection>
+            </SidebarBody>
+            <SidebarFooter className="max-lg:hidden">
+              <Dropdown>
+                <DropdownButton as={SidebarItem}>
+                  <span className="flex min-w-0 items-center gap-3">
+                    <Avatar src="/profile-photo.jpg" className="size-10" square alt="" />
+                    <span className="min-w-0">
+                      <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">Erica</span>
+                      <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
+                        erica@example.com
+                      </span>
+                    </span>
+                  </span>
+                  <ChevronUpIcon />
+                </DropdownButton>
+                <DropdownMenu className="min-w-64" anchor="top start">
+                  <DropdownItem href="/my-profile">
+                    <UserIcon />
+                    <DropdownLabel>My profile</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownItem href="/settings">
+                    <Cog8ToothIcon />
+                    <DropdownLabel>Settings</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownDivider />
+                  <DropdownItem href="/privacy-policy">
+                    <ShieldCheckIcon />
+                    <DropdownLabel>Privacy policy</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownItem href="/share-feedback">
+                    <LightBulbIcon />
+                    <DropdownLabel>Share feedback</DropdownLabel>
+                  </DropdownItem>
+                  <DropdownDivider />
+                  <DropdownItem href="/logout">
+                    <ArrowRightStartOnRectangleIcon />
+                    <DropdownLabel>Sign out</DropdownLabel>
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </SidebarFooter>
+          </Sidebar>
+        }
+      >
+        {children}
+      </SidebarLayout>
     );
 }
